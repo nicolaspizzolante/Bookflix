3fcbc334fdde743a66d3070621e8c6f1a4190687
nicolaspizzolante <nicopizzolante@gmail.com>
2020-05-02 18:35:39 -0300

    fix validar password

--- validarPassword.php
+++ validarPassword.php
@@ -3,11 +3,8 @@ function validar_password($password,&$error_password){
 	$error_password = "";
   
   $pattern = '/[\'\/~`\!@#$%\^&\*\(\)_\-\+=\{\}\[\]\|;:"\<\>,\.\?\\]/';
-  if (!((preg_match('`[0-9]`',$password)) or (preg_match($pattern, $password))) or 
-    (strlen($password) < 6) or
-    (!preg_match('`[a-z]`',$password)) or 
-    (!preg_match('`[A-Z]`',$password))) {
-    $error_password .= "<li>La contraseña debe tener al menos 6 caracteres, una mayuscula, una minuscula y un numero o un simbolo.</li>";
+  if (!((preg_match('`[0-9]`',$password)) or (preg_match($pattern, $password))) or (strlen($password) < 6)) {
+    $error_password .= "<li>La contraseña debe tener al menos 8 alfanumericos.</li>";
   }
 
   if($error_password){
