35e6ca2772100700079e815d18fe8693f6270641
cololaborde <cololaborde@gmail.com>
2020-05-10 21:04:10 -0300

    fix validarEdicionMetadatos

--- validarEdicionMetadatos.php
+++ validarEdicionMetadatos.php
@@ -18,7 +18,7 @@ if ($_SESSION['errores']){
 }
 
 //consulta para saber si el libro ya existe en la db
-$sql = "SELECT id FROM libros WHERE isbn = '$isbn' AND id != '$id'";
+$sql = "SELECT id FROM libros WHERE isbn = '$isbn' AND id <> '$id'";
 $resultado = $conexion->query($sql);
 $libro = $resultado->fetch_assoc();
 
