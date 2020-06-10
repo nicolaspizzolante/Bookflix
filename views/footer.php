de002425a264b47ac9c23a7f152cc13ac4012a51
GustavoRu <gustavoruiz.gr96@gmail.com>
2020-06-10 15:21:36 -0300

    busqueda

--- views/footer.php
+++ views/footer.php
@@ -4,5 +4,8 @@
 			</div>
 		<?php } ?>
 	<script src="js/validacion.js"></script>
+	<script src="js/scripts.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="js/sweetalert2.all.min.js"></script>
 </body>
 </html>
