29eca349e50c035ff0fa2bf694237f77d2407a22
nicolaspizzolante <nicopizzolante@gmail.com>
2020-06-10 22:50:54 -0300

    add leer libro

---
+++ leerLibro.php
@@ -0,0 +1,184 @@
+<?php
+	include 'autenticador.php';
+	$autenticador = new autenticador();
+	
+	if (!$autenticador->estaLogeado() || !$autenticador->esAdmin()) {
+		header('Location: login.php');
+		exit;
+	} 
+    include 'views/header.php';
+
+    $db = conectar();
+    $libro_id = $_GET['id'];
+
+    $sql = "SELECT pdf FROM libros_pdf WHERE id = $libro_id";
+    $resultado = $db->query($sql);
+    $libro = $resultado->fetch_assoc()['pdf'];
+
+?>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.top-bar {
+  background: #333;
+  color: #fff;
+  padding: 1rem;
+}
+
+.btn {
+  background: coral;
+  color: #fff;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 0.7rem 2rem;
+}
+
+.btn:hover {
+  opacity: 0.9;
+}
+
+.page-info {
+  margin-left: 1rem;
+}
+
+.error {
+  background: orangered;
+  color: #fff;
+  padding: 1rem;
+}
+</style>
+
+
+<div class="top-bar">
+<button class="btn" id="prev-page">
+<i class="fas fa-arrow-circle-left"></i> Prev Page
+</button>
+<button class="btn" id="next-page">
+Next Page <i class="fas fa-arrow-circle-right"></i>
+</button>
+<span class="page-info">
+Page <span id="page-num"></span> of <span id="page-count"></span>
+</span>
+</div>
+
+<div style="display:flex; justify-content:center;">
+<canvas id="pdf-render"></canvas>
+</div>
+<script src="http://mozilla.github.io/pdf.js/build/pdf.js"></script>
+
+<script>
+var BASE64_MARKER = ';base64,';
+
+function convertDataURIToBinary(dataURI) {
+  var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+  var base64 = dataURI.substring(base64Index);
+  var raw = window.atob(base64);
+  var rawLength = raw.length;
+  var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+  for(var i = 0; i < rawLength; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+  return array;
+}
+
+var pdfAsDataUri = "data:application/pdf;base64,<?php echo base64_encode($libro) ?>";
+var pdfAsArray = convertDataURIToBinary(pdfAsDataUri);
+
+let pdfDoc = null,
+  pageNum = 1,
+  pageIsRendering = false,
+  pageNumIsPending = null;
+
+const scale = 1.5,
+  canvas = document.querySelector('#pdf-render'),
+  ctx = canvas.getContext('2d');
+
+// Render the page
+const renderPage = num => {
+  pageIsRendering = true;
+
+  // Get page
+  pdfDoc.getPage(num).then(page => {
+    // Set scale
+    const viewport = page.getViewport({ scale });
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    const renderCtx = {
+      canvasContext: ctx,
+      viewport
+    };
+
+    page.render(renderCtx).promise.then(() => {
+      pageIsRendering = false;
+
+      if (pageNumIsPending !== null) {
+        renderPage(pageNumIsPending);
+        pageNumIsPending = null;
+      }
+    });
+
+    // Output current page
+    document.querySelector('#page-num').textContent = num;
+  });
+};
+
+// Check for pages rendering
+const queueRenderPage = num => {
+  if (pageIsRendering) {
+    pageNumIsPending = num;
+  } else {
+    renderPage(num);
+  }
+};
+
+// Show Prev Page
+const showPrevPage = () => {
+  if (pageNum <= 1) {
+    return;
+  }
+  pageNum--;
+  queueRenderPage(pageNum);
+};
+
+// Show Next Page
+const showNextPage = () => {
+  if (pageNum >= pdfDoc.numPages) {
+    return;
+  }
+  pageNum++;
+  queueRenderPage(pageNum);
+};
+
+// Get Document
+pdfjsLib
+  .getDocument(pdfAsArray)
+  .promise.then(pdfDoc_ => {
+    pdfDoc = pdfDoc_;
+
+    document.querySelector('#page-count').textContent = pdfDoc.numPages;
+
+    renderPage(pageNum);
+  })
+  .catch(err => {
+    // Display error
+    const div = document.createElement('div');
+    div.className = 'error';
+    div.appendChild(document.createTextNode(err.message));
+    document.querySelector('body').insertBefore(div, canvas);
+    // Remove top bar
+    document.querySelector('.top-bar').style.display = 'none';
+  });
+
+// Button Events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
+
+
+</script>
