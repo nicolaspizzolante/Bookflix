e0b6dfe2853e8253a0eafa8b0b6a61a8a158851b
nicolaspizzolante <nicopizzolante@gmail.com>
2020-07-10 15:48:52 -0300

    add bajaperfil.php

--- administrarperfiles.php
+++ administrarperfiles.php
@@ -11,13 +11,62 @@
     
     $usuario_id = $_SESSION['usuario']['id'];
 
+	// traigo todos los perfiles del usuario
 	$db = conectar();
 	$sql = "SELECT * FROM perfiles WHERE usuario_id = '$usuario_id'";
     $resultado = $db->query($sql);
 
-    while ($perfil = $resultado->fetch_assoc()){
-        var_dump($perfil);
-    }
 ?>
+
+<style>
+
+.perfiles {
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+}
+
+.perfil {
+	width: 500px;
+	padding: 20px;
+	background: #151515;
+	display: flex;
+	justify-content: space-between;
+	align-items:center;
+	border-radius:3px;
+	margin-bottom: 10px;
+}
+
+a {
+	color: white !important;
+}
+
+</style>
+
+<div class="container">
+
+	<h1>Tus perfiles</h1>
+
+	<div class="perfiles">
+		<?php while ($perfil = $resultado->fetch_assoc()){ ?>
+			<div class="perfil">
+				<h3> <?= $perfil['nombre'] ?></h3>
+
+				<div class="botones">
+					<button class="btn-eliminar"><a href="bajaperfil.php?id=<?= $perfil['id'] ?>&usuario_id=<?= $perfil['usuario_id'] ?>">Eliminar</a></button>
+				</div>
+			</div>
+		<?php } ?>
+	</div>
+
+	<?php if (isset($_SESSION['errores'])): ?>
+		<ul id="errores" class="errores_mensaje" style="display:block;">
+			<?php 
+				echo $_SESSION['errores']; 
+				unset($_SESSION['errores']);
+			?>
+		</ul>
+	<?php endif ?>
+</div>
 
 <?php include 'views/footer.php'; ?>
---
+++ bajaPerfil.php
@@ -0,0 +1,31 @@
+<?php 
+session_start();
+include 'db.php';
+
+$db = conectar();
+$id = $_GET['id'];
+$usuario_id = $_GET['usuario_id'];
+
+$sql = "SELECT count(*) as cant FROM perfiles WHERE usuario_id = $usuario_id";
+$resultado = $db->query($sql);
+$cant = $resultado->fetch_assoc()['cant'];
+
+// contar perfiles, si solo le queda uno no dejarlo borrar, devolverlo a administrarperfiles.php
+if($cant == 1) {
+	$_SESSION['errores'] = '<li>Solo te queda un perfil, no lo podes borrar.</li>';
+
+    header("Location: administrarperfiles.php");
+} else {
+    $sql = "DELETE FROM perfiles WHERE id = $id";
+    $db->query($sql);
+    
+	$_SESSION['exito'] = '<li>Perfil borrado, volve a logearte.</li>';
+
+    session_destroy();
+    
+    header("Location: login.php");
+}
+
+
+
+?>
