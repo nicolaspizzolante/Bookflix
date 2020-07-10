78e165df6018c86149d5f0e390ff5b1fe144f46e
cololaborde <64672548+cololaborde@users.noreply.github.com>
2020-07-10 14:28:00 -0300

    update listado reportes

--- verReportes.php
+++ verReportes.php
@@ -15,8 +15,30 @@
 	$conexion = conectar();
 
 	?>
-	<h3>Que reporte desea obtener?</h3>
-	<a href="masLeidos.php">Libros en orden de lecturas</a>
-	<a href="suscriptosEntreFechas.php">Usuarios suscriptos entre fechas</a>
+	<h2 style="text-align:center;margin:10px;color:#E51F1F">Que reporte desea obtener?</h2>
+
+	<article class="libro">
+				<div class="info">
+					<div class="titulo">
+						<h3 style="margin-left: 520px">
+						<a href="masLeidos.php">Libros en orden de lecturas</a>
+						</h3>
+					</div>
+				</div>
+		</article>
+
+		<article class="libro">
+				<div class="info">
+					<div class="titulo">
+						<h3 style="margin-left: 520px">
+						<a href="suscriptosEntreFechas.php" style="align-text:center">Usuarios suscriptos entre fechas</a>
+						</h3>
+					</div>
+				</div>
+		</article>
+
+	
+	
+	
 		
 <?php  include 'views/footer.php'; ?>
