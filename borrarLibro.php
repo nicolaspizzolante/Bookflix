d451ed501d71cffe567cd29d51a7f17030a6b986
cololaborde <cololaborde@gmail.com>
2020-07-17 14:27:29 -0300

    fix

--- borrarLibro.php
+++ borrarLibro.php
@@ -8,9 +8,10 @@ $idpdf = isset($_GET['pdf_id']) ? $_GET['pdf_id'] : NULL;
 if(!$idpdf){
     $sql = "DELETE FROM libros WHERE id = $idLibro";
     $db->query($sql);
+
     
-    
-    header("Location: perfilLibro.php?id=$idLibro");
+
+    //header("Location: perfilLibro.php?id=$idLibro");
 }else{
     $sql = "DELETE FROM libros_pdf WHERE id = $idpdf";
     $db->query($sql);
@@ -24,6 +25,17 @@ if(!$idpdf){
     if($cantSubidos == 0){
         $sql = "UPDATE libros SET capitulos = 0 WHERE id=$idLibro";
         $db->query($sql);
+
+        $sql = "SELECT * FROM historial WHERE libro_id = $idLibro";
+        $r = $db->query($sql);
+        while ($histo = $r->fetch_assoc()){
+            if(!$histo['terminado']){
+                $h_perfil_id = $histo['perfil_id'];
+                $sql = "DELETE FROM historial WHERE libro_id = $idLibro and perfil_id = $h_perfil_id";
+                $db->query($sql);
+            }
+    }
+
     }
 
     header("Location: libro.php?id=$idLibro");
